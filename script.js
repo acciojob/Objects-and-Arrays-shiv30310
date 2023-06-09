85b3bdfce5f507e76a52029ff6789b48ee105a5e
Gulshan Soni <43094983+shiv30310@users.noreply.github.com>
2023-06-09 19:16:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const person = {
   name: "John Doe",
   age: 80,
 };
+let team  = players
+let team1  = players.map((player) => player)
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-
+let cap1 = [...person]
 window.players = players;
 window.person = person;
 window.team = team;
